ffb698180763f5411cc40befd7abcbe736fd34b2
Firstp1ck <kortharshadowbreath@gmail.com>
2024-03-22 08:49:24 +0100

    updated filenames

--- PDF_Converter_Laptop.bat
+++ PDF_Converter_Laptop.bat
@@ -1,3 +1,3 @@
 @echo off
-python "C:\Users\alleu\OneDrive\Dokumente\GitHub\PDF-Converter"
+python "C:\Users\alleu\OneDrive\Dokumente\GitHub\PDF-Converter\PDF_Converter_01.py"
 pause
--- PDF_Converter_PC home.bat
+++ PDF_Converter_PC home.bat
@@ -1,3 +1,3 @@
 @echo off
-python "D:\Benutzer\Firstpick\Python\Projekte\PDF-Converter\PDF converter_01.py"
+python "D:\Benutzer\Firstpick\Python\Projekte\PDF-Converter\PDF_converter_01.py"
 pause
